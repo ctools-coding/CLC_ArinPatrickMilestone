0fb8eedf198e4ac0b853f0132bc3992862a2f639
airisrare <Pulmer@my.gcu.edu>
2021-02-21 17:59:38 -0700

    these changes mean nothing

--- CLC_ArinPatrick/Controllers/MinesweeperController.cs
+++ CLC_ArinPatrick/Controllers/MinesweeperController.cs
@@ -4,13 +4,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
+
 namespace Minesweeper_ArinPatrick.Controllers
 {
     public class MinesweeperController : Controller
     {
         public IActionResult Index()
         {
-            return View();
+          
         }
 
     }
