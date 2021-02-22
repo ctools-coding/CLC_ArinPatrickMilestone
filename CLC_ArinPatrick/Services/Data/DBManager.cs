76052637f01643438177b236d86ce57158288132
airisrare <Pulmer@my.gcu.edu>
2021-02-21 18:19:36 -0700

    Moved the data layer to the services->data folder

---
+++ CLC_ArinPatrick/Services/Data/DBManager.cs
@@ -0,0 +1,195 @@
+﻿using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
+using System;
+using System.Text;
+
+namespace DataAccess_Layer
+{
+    public class DBManager
+    {
+        //defining our class attribute
+        public string dbUserconn { get; set; }
+
+        public DBManager()
+        {
+            this.dbUserconn = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=dbMinesweeper;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+        }
+
+        /// <summary>
+        /// Gets all users from the database
+        /// </summary>
+        /// <returns></returns>
+        public IEnumerable<UserModel> GetAllUsers()
+        {
+           List<UserModel> userList = new List<UserModel>();
+           using (SqlConnection conn = new SqlConnection(dbUserconn))
+           {
+                using (SqlCommand cmd = new SqlCommand("usp_GetAllUsers", conn))
+                {
+                    //By setting the command type to stored procedure, the first parameter
+                    //to the SQL command constructor will be interpreted as the name of a stored procedure
+                    //instead of interpreting a command string 
+                    //which would usually be the sql query 
+
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    conn.Open();
+
+                    //this provides a way of reading a forward only steam of rows from 
+                    //a SQL server database
+                    SqlDataReader dataReader = cmd.ExecuteReader();
+                    //Iterate through all the data rows that were read 
+                    while (dataReader.Read())
+                    {
+                        //Instantiate our EmployeeInfo class creating an object called emp
+                        UserModel user = new UserModel();
+                        try
+                        {
+                            user.UserID = Convert.ToInt32(dataReader["UserID"].ToString());
+                        }
+                        catch
+                        {
+                            user.UserID = 0;
+                        }
+
+                        user.First = dataReader["First"].ToString();
+                        user.Last = dataReader["Last"].ToString();
+                        user.Gender = dataReader["Gender"].ToString();
+                        user.Age = Convert.ToInt32(dataReader["Age"]);
+                        user.State = dataReader["State"].ToString();
+                        user.Email = dataReader["Email"].ToString();
+                        user.Username = dataReader["Username"].ToString();
+                        user.Password = dataReader["Password"].ToString();
+
+                        //Add (commit) this row of data to our list
+                        userList.Add(user);
+                    }
+                }
+                //even though using should clean this up
+                //it is goo practice to close the connection
+                conn.Close();
+           }
+             return userList;
+        }
+
+        public UserModel GetUserByID(int userID)
+        {
+            UserModel user = new UserModel();
+            using (SqlConnection conn = new SqlConnection(dbUserconn))
+            {
+                string findCmd = @"SELECT * FROM Users WHERE UserID = @userID";
+              
+                using (SqlCommand cmd = new SqlCommand(findCmd, conn))
+                {
+                    conn.Open();
+               
+                    SqlDataReader dataReader = cmd.ExecuteReader();
+
+                    //Iterate through all the data rows that were read 
+                    while (dataReader.Read())
+                    {
+                        //Instantiate our usermodel class creating an object called user
+                        try
+                        {
+                            user.UserID = Convert.ToInt32(dataReader["UserID"].ToString());
+                        }
+                        catch
+                        {
+                            user.UserID = 0;
+                        }
+
+                        user.First = dataReader["First"].ToString();
+                        user.Last = dataReader["Last"].ToString();
+                        user.Gender = dataReader["Gender"].ToString();
+                        user.Age = Convert.ToInt32(dataReader["Age"]);
+                        user.State = dataReader["State"].ToString();
+                        user.Email = dataReader["Email"].ToString();
+                        user.Username = dataReader["Username"].ToString();
+                        user.Password = dataReader["Password"].ToString();
+                    }
+                }
+                //even though using should clean this up
+                //it is goo practice to close the connection
+                conn.Close();
+            }
+            return user;
+        }
+        /// <summary>
+        /// Gets user based off of given username and password
+        /// </summary>
+        /// <param name="user"></param>
+        /// <returns></returns>
+        public bool GetUserLogin(UserModel user)
+        {
+            bool success = false;
+            using (SqlConnection conn = new SqlConnection(dbUserconn))
+            {
+                string sqlStmt = @"SELECT * FROM Users WHERE Username = @username AND Password = @password";
+
+                using (SqlCommand cmd = new SqlCommand(sqlStmt, conn))
+                {
+                    cmd.Parameters.Add("@Username", System.Data.SqlDbType.NVarChar, 25).Value = user.Username;
+                    cmd.Parameters.Add("@Password", System.Data.SqlDbType.NVarChar, 25).Value = user.Password;
+
+                    //Iterate through all the data rows that were read 
+                    try
+                    {
+                        conn.Open();
+                        SqlDataReader reader = cmd.ExecuteReader();
+                        if(reader.HasRows)
+                        {
+                            success = true;
+                        }
+
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine(e.Message);
+                    }
+                }
+                //even though using should clean this up
+                //it is goo practice to close the connection
+                conn.Close();
+            }
+            return success;
+        }
+
+        /// <summary>
+        /// Adds a user to the database from the model info given
+        /// </summary>
+        /// <param name="user"></param>
+        /// <returns></returns>
+        public bool AddUser(UserModel user)
+        {
+            bool isSuccessful = true;
+            using (SqlConnection conn = new SqlConnection(dbUserconn))
+            {
+                // If you didn't wan to use Stored Procedures this is a second example
+                // SqlCommand cmd = new SqlCommand(insertCmd, conn)
+                // CommandType would not be included
+                using (SqlCommand cmd = new SqlCommand("usp_InsertUser", conn))
+                {
+                    cmd.CommandType = CommandType.StoredProcedure;
+
+                    // Open up the stored procedure and review the parameters
+                    // required to be passed into the procdure
+                    cmd.Parameters.AddWithValue("@First", user.First);
+                    cmd.Parameters.AddWithValue("@Last", user.Last);
+                    cmd.Parameters.AddWithValue("@Gender", user.Gender);
+                    cmd.Parameters.AddWithValue("@Age", user.Age);
+                    cmd.Parameters.AddWithValue("@State", user.State);
+                    cmd.Parameters.AddWithValue("@Email", user.Email);
+                    cmd.Parameters.AddWithValue("@Username", user.Username);
+                    cmd.Parameters.AddWithValue("@Password", user.Password);
+
+                    conn.Open();
+                    // ExecuteNonQuery used for executing queries that don't return data
+                    // Used for update, insert and delete
+                    cmd.ExecuteNonQuery();
+                }
+                conn.Close();
+            }
+            return isSuccessful;
+        }
+    }
+}
---
+++ CLC_ArinPatrick/Services/Data/UserModel.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace DataAccess_Layer
+{
+    //no ur mom
+    public class UserModel
+    {
+        //ID is pk
+        public int UserID { get; set; }
+
+        [Display(Name = "First Name")]
+        [Required(ErrorMessage = "Please enter your first name.")]
+        public string First { get; set; }
+
+        [Display(Name = "Last Name")]
+        [Required(ErrorMessage = "Please enter your last name.")]
+        public string Last { get; set; }
+
+        [DataType(DataType.Text)]
+        [Display(Name = "Gender")]
+        [Required(ErrorMessage = "Please enter your gender.")]
+        public string Gender { get; set; }
+
+        [DataType(DataType.Text)]
+        [Display(Name = "Age")]
+        [Range(0,200)]
+        [Required(ErrorMessage = "Please enter your age.")]
+        public int Age { get; set; }
+
+        [DataType(DataType.Text)]
+        [Display(Name = "State")]
+        [StringLength(2, ErrorMessage = "Please enter state using shorthand. (ex. HI, AZ)")]
+        [Required(ErrorMessage = "Please enter your state.")]
+        public string State { get; set; }
+
+        [DataType(DataType.Text)]
+        [Display(Name = "Email")]
+        [Required(ErrorMessage = "Please enter your email.")]
+        public string Email { get; set; }
+
+        [DataType(DataType.Text)]
+        [Display(Name = "Username")]
+        [Required(ErrorMessage = "Please enter your Username.")]
+        public string Username { get; set; }
+
+        [DataType(DataType.Password)]
+        [Display(Name = "Password")]
+        [Required(ErrorMessage = "Please enter your gender.")]
+        [StringLength(20, MinimumLength = 5, ErrorMessage="Password must be at least 5 characters.")]
+        public string Password { get; set; }
+    }
+}
