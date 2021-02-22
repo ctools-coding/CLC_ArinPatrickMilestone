527a90d10518cef1f60eb18b486e14553bba4549
airisrare <Pulmer@my.gcu.edu>
2021-02-21 17:30:15 -0700

    Game play driver

--- Business Layer/Game.cs
+++ Business Layer/Game.cs
@@ -1,181 +1,241 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Text;
+using Minesweeper;
 using System.Text;
+using System.Windows.Forms;
+using System.Diagnostics;
 
-namespace Business_Layer
+namespace MinesweeperGUI
 {
-    class Game
-    {
+    public partial class Form2 : Form
+    {//create buttons
+        public Button[,] btnGame;
+        //game size int
+        public int GameSize;
 
-            /// <summary>
-            /// playable game method, asks user for input (size and difficulty) 
-            /// </summary>
-            /// <param name="args"></param>
-            static void Main(string[] args)
+        //board size of zero, user picks size
+        Board board = new Board(0);
+
+        //global variable for game over
+        public int isGameOver = 0;
+
+        //variable for diffictuly
+        public int difficulty;
+        //variable for score
+        int mineScore;
+
+
+        //stopwatch
+        public Stopwatch stopWatch;
+
+        //form two takes the variables passed in form one( difficulty and size) 
+        public Form2(int gameDifficulty, int gameSize)
+        {
+            InitializeComponent();
+            GameSize = gameSize;
+            //use difficulty later in the form, so difficulty = game difficulty 
+            difficulty = gameDifficulty;
+
+            //btn  game is button] 
+            btnGame = new Button[gameSize, gameSize];
+
+            board = new Board(gameSize);
+
+            //board sets up live neighbors, the more live neighbors, the more bombs. !!
+            board.setUpLiveNeighbors(gameDifficulty);
+            //calculate live neighbors is the actual number that is  on each cell.
+            board.calculateLiveNeighbors();
+
+            populateGame(gameSize);
+
+
+        }
+
+        /// <summary>
+        /// creates the game board
+        /// </summary>
+        /// <param name="boardGameSize"></param>
+        public void populateGame(int boardGameSize)
+        {
+            //makes the button width the size of the board game
+            int buttonSize = pnl_gameboard.Width / boardGameSize;
+            //creates square buttons
+            pnl_gameboard.Height = pnl_gameboard.Width;
+
+            //create new buttons and place them into the panel
+            //NS = north south = column
+            //EW = East West = row
+            for (int EW = 0; EW < boardGameSize; EW++)
             {
-                Console.WriteLine("Between 1-12, pick the size of the square board you would like : ");
-                //Reads what person puts in console
-                int size = int.Parse(Console.ReadLine());
-                Board board = new Board(size);
-                Console.WriteLine("Between 1-50, how difficult would you like the game to be (recommended 15): ");
-                //reads difficulty
-                board.Difficulty = int.Parse(Console.ReadLine());
-
-                board.setUpLiveNeighbors(board.Difficulty);
-                board.calculateLiveNeighbors();
-
-
-                //if you want, remove comment // and this will give you the unrevieled board
-                printBoard(board);
-                Console.ReadKey();
-
-
-
-                //local variables for the game over loop
-                bool GameOver = false;
-                int RowNumber;
-                int ColumnNumber;
-
-                while (!GameOver)
+                for (int NS = 0; NS < boardGameSize; NS++)
                 {
-                    printBoardDuringGame(board);
-                    Console.WriteLine("Please enter a column number: ");
-                    //user input
-                    ColumnNumber = int.Parse(Console.ReadLine());
+                    btnGame[EW, NS] = new Button();
+                    //width and height of each column
+                    btnGame[EW, NS].Width = buttonSize;
+                    btnGame[EW, NS].Height = buttonSize;
+                    btnGame[EW, NS].MouseUp += Form2_MouseUp;
+                    //put the buttons on the panel
+                    pnl_gameboard.Controls.Add(btnGame[EW, NS]);
+                    btnGame[EW, NS].Location = new Point(buttonSize * NS, buttonSize * EW);
 
-                    Console.WriteLine("Please enter a row number: ");
-                    // user input
-                    RowNumber = int.Parse(Console.ReadLine());
-
-
-                    //revealing the cell
-                    board.grid[RowNumber - 1, ColumnNumber - 1].Visited = true;
-
-                    board.FloodFill(RowNumber - 1, ColumnNumber - 1);
-
-
-                    GameOver = IsGameOver(board, RowNumber, ColumnNumber);
-
+                    btnGame[EW, NS].Name = EW.ToString() + "|" + NS.ToString();
                 }
-                //final board(key) when the game is over
-                printBoard(board);
-                Console.ReadKey();
             }
 
-            /// <summary>
-            /// Checks and sees if the game is over, either the user hits a bomb, or their are no more squares to reveal that arent bombs
-            /// </summary>
-            /// <param name="board"></param>
-            /// <param name="row"></param>
-            /// <param name="col"></param>
-            /// <returns></returns>
-            static bool IsGameOver(Board board, int row, int col)
+        }
+
+        /// <summary>
+        /// right button click event
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Form2_MouseUp(object sender, MouseEventArgs e)
+        {
+            //array of buttons. the array allows the board to know what button we are going to click
+            string[] Sarr = (sender as Button).Name.ToString().Split('|');
+            int BtnRow = int.Parse(Sarr[0]);
+            int BtnCol = int.Parse(Sarr[1]);
+            board.grid[BtnRow, BtnCol].Visited = true;
+            isGameOver = board.IsGameOverGUI(BtnRow, BtnCol);
+
+            //right button click puts a flag on the button.
+            if (e.Button == System.Windows.Forms.MouseButtons.Right && btnGame[BtnRow, BtnCol].BackgroundImage == null)
             {
-                //return breaks the loop 
-                if (board.grid[row - 1, col - 1].Visited == true && board.grid[row - 1, col - 1].Live == true)
+                btnGame[BtnRow, BtnCol].BackgroundImage = Image.FromFile(@"..\pictures\Flag.jpg");
+                btnGame[BtnRow, BtnCol].BackgroundImageLayout = ImageLayout.Stretch;
+            }
+            //if we right button click the flag again, it turns the flag cell to null
+            else if (e.Button == System.Windows.Forms.MouseButtons.Right)
+            {
+                btnGame[BtnRow, BtnCol].BackgroundImage = null;
+            }
+
+            //left button click actually clicks the cell.
+            if (e.Button == System.Windows.Forms.MouseButtons.Left)
+            {
+                // int 0 means that the user selected a bomb and the game is over.
+                if (isGameOver == 0)
                 {
-                    Console.WriteLine("You exploded");
-                    return true;
-                }
-                for (int i = 0; i < board.Size; i++)
-                {
-                    for (int j = 0; j < board.Size; j++)
+                    for (int row = 0; row < GameSize; row++)
                     {
-                        if (board.grid[i, j].Visited == false && board.grid[i, j].Live == false)
+                        for (int col = 0; col < GameSize; col++)
                         {
-                            return false;
+                            btnGame[row, col].Text = board.grid[row, col].LiveNeighbors.ToString();
+                            btnGame[row, col].BackColor = Color.Black;
+                            btnGame[row, col].ForeColor = Color.Red;
+
+                            //bomb image
+                            if (board.grid[row, col].Live)
+                            {
+                                btnGame[row, col].BackgroundImage = Image.FromFile(@"..\pictures\Bomb.jpg");
+                                btnGame[row, col].BackgroundImageLayout = ImageLayout.Stretch;
+                            }
                         }
                     }
+                    // when the game is over the game and the timer stops
+                    stopWatch.Stop();
+                    this.BackColor = Color.Red;
+
+                    MessageBox.Show("I think I am cool and thats all that matters. \n -Tyler the creator \n   You survived for: " + lbl_stopwatch.Text);
+                    //score shows 0 because they lost
+                    Highscores form3 = new Highscores((mineScore * 0), difficulty, GameSize, stopWatch.Elapsed.TotalSeconds);
+                    form3.ShowDialog();
                 }
-                //once we have no more visited cells that are not bombs, we win the game
-                Console.WriteLine("Congrats you won the game");
-                return true;
-
-
-            }
-
-            /// <summary>
-            /// this print board method is the key, this is a revealed board
-            /// </summary>
-            /// <param name="board"></param>
-            static void printBoard(Board board)
-            {
-                //sets up the top numbers
-                for (int i = 0; i < board.Size; i++)
+                //int 100 is when the user wins the game
+                else if (isGameOver == 100)
                 {
-                    Console.Write("  " + (i + 1) + "  ");
-                }
-                Console.WriteLine();
-                for (int i = 0; i < board.Size; i++)
-                {
-                    Console.WriteLine();
-
-                    //middle of the board with number of live neighbors inside
-
-                    for (int j = 0; j < board.Size; j++)
+                    for (int row = 0; row < GameSize; row++)
                     {
-                        Console.Write("| " + board.grid[i, j].LiveNeighbors + " |");
-                    }
-
-                    //column numbers
-                    Console.Write(i + 1);
-
-                    Console.WriteLine();
-
-                }
-            }
-            /// <summary>
-            /// this is the game board that is playable, squares are hidden for the user to reveal, squares that have no Live neighbors are marked with "~"
-            /// </summary>
-            /// <param name="board"></param>
-            static void printBoardDuringGame(Board board)
-            {
-                //top numbers
-                for (int i = 0; i < board.Size; i++)
-                {
-                    Console.Write("  " + (i + 1) + "  ");
-                }
-                Console.WriteLine();
-
-                for (int i = 0; i < board.Size; i++)
-                {
-
-                    for (int j = 0; j < board.Size; j++)
-                    {
-                        //fills the board with ? marks if the cell is not visited
-                        if (board.grid[i, j].Visited == false)
+                        for (int col = 0; col < GameSize; col++)
                         {
-                            Console.Write("| ? |");
-                        }
+                            //the buttons turns green when the user wins
+                            btnGame[row, col].Text = board.grid[row, col].LiveNeighbors.ToString();
 
-                        if (board.grid[i, j].LiveNeighbors == 0 && board.grid[i, j].Live == false && board.grid[i, j].Visited == true)
-                        {
-                            Console.Write("| ~ |");
-                        }
+                            btnGame[row, col].BackColor = Color.Green;
+                            btnGame[row, col].ForeColor = Color.White;
 
-                        //mark the board with * as a bomb if it is visited and if it is a bomb
-                        if (board.grid[i, j].Visited == true && board.grid[i, j].Live == true)
-                        {
-                            Console.Write("| * |");
-                        }
+                            if (board.grid[row, col].Live)
+                            {
+                                btnGame[row, col].BackgroundImage = Image.FromFile(@"..\pictures\Flag.jpg");
+                                btnGame[row, col].BackgroundImageLayout = ImageLayout.Stretch;
 
-                        //if the cell is visited, but no bomb, we get the live neighbors around the cell 
-                        if (board.grid[i, j].Visited == true && board.grid[i, j].Live == false && board.grid[i, j].LiveNeighbors != 0)
-                        {
-
-                            Console.Write("| " + board.grid[i, j].LiveNeighbors + " |");
+                            }
                         }
                     }
 
+                    //difficulty score calculator and formula, higher difficulty = higher score
+                    if (difficulty <= 9)
+                    {
+                        mineScore = (int)(((50 * GameSize * difficulty) - stopWatch.Elapsed.TotalSeconds));
+                    }
+                    if (difficulty >= 10 && difficulty <= 13)
+                    {
+                        mineScore = (int)(((500 * GameSize * difficulty) - stopWatch.Elapsed.TotalSeconds));
+                    }
+                    if (difficulty >= 14 && difficulty <= 16)
+                    {
+                        mineScore = (int)(((5000 * GameSize * difficulty) - stopWatch.Elapsed.TotalSeconds));
+                    }
+                    if (difficulty >= 17 && difficulty <= 20)
+                    {
+                        mineScore = (int)(((50000 * GameSize * difficulty) - stopWatch.Elapsed.TotalSeconds));
+                    }
 
+                    this.BackColor = Color.Green;
+                    //the timer stops and a win message is shown.
+                    stopWatch.Stop();
+                    MessageBox.Show("Good Job,\n Tyler would be happy\nYou took " + lbl_stopwatch.Text + " to figure out the game. \n Your score is " + mineScore);
 
-
-                    //row numbers
-                    Console.Write(i + 1);
-                    Console.WriteLine();
+                    Highscores form3 = new Highscores(mineScore, difficulty, GameSize, stopWatch.Elapsed.TotalSeconds);
+                    form3.ShowDialog();
 
                 }
-                Console.WriteLine();
+                //continue method
+                else
+                {
+                    //calling the flood fill recursive method in the board class
+                    board.FloodFill(BtnRow, BtnCol);
+
+                    for (int row = 0; row < GameSize; row++)
+                    {
+                        for (int col = 0; col < GameSize; col++)
+                        {
+                            if (board.grid[row, col].Visited)
+                            {
+                                btnGame[row, col].Text = board.grid[row, col].LiveNeighbors.ToString();
+
+                            }
+                        }
+                    }
+                }
+
             }
         }
+
+        /// <summary>
+        /// when our form 2 loads, we start the timer
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Form2_Load(object sender, EventArgs e)
+        {
+            stopWatch = new Stopwatch();
+            stopWatch.Start();
+        }
+
+        /// <summary>
+        /// timer format
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            //concantinates formatted string for stopwatch
+            this.lbl_stopwatch.Text = string.Format("{0:hh\\:mm\\:ss\\.ff}", stopWatch.Elapsed);
+        }
+    }
 }
