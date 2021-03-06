578c9091fc04f3e3c1a122bbe1fcd5a118b834b9
airisrare <Pulmer@my.gcu.edu>
2021-03-05 23:02:59 -0700

    Right mouse down button event working

--- CLC_ArinPatrick/Controllers/MinesweeperController.cs
+++ CLC_ArinPatrick/Controllers/MinesweeperController.cs
@@ -10,13 +10,13 @@ namespace Minesweeper_ArinPatrick.Controllers
     public class MinesweeperController : Controller
     {
         public static Board board;
+        List<Cell> cellList = new List<Cell>();
         public IActionResult Index()
         {
             board = new Board(8);
             board.setUpLiveNeighbors(7);
             board.calculateLiveNeighbors();
 
-            List<Cell> cellList = new List<Cell>();
 
             foreach(Cell cell in board.grid)
             {
@@ -43,8 +43,28 @@ namespace Minesweeper_ArinPatrick.Controllers
                 cellList.Add(cell);
             }
 
+            //testing
+            //board.grid[0, 0].Flagged = true;
+
             ViewBag.row = row;
             ViewBag.col = col;
+            return View("Index", cellList);
+        }
+
+        public IActionResult OnRightButtonClick(string location)
+        {
+            string[] coordinates = location.Split(',');
+            int row = int.Parse(coordinates[0]);
+            int col = int.Parse(coordinates[1]);
+
+            board.grid[row,col].Flagged = !board.grid[row,col].Flagged;
+
+            List<Cell> cellList = new List<Cell>();
+
+            foreach (Cell cell in board.grid)
+            {
+                cellList.Add(cell);
+            }
             return View("Index", cellList);
         }
 
