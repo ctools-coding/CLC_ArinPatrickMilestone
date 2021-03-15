ed70b9a1035a8924118e6414a1dbd9da1a862826
Arin Aihara <AAihara@my.gcu.edu>
2021-03-14 17:57:06 -0700

    js and partial pages progress

--- CLC_ArinPatrick/wwwroot/js/site.js
+++ CLC_ArinPatrick/wwwroot/js/site.js
@@ -1,1 +1,48 @@
-﻿
+﻿$(function ()
+{
+    console.log("Page is loaded.");
+    $(document).on("click", ".game-button", function (event)
+    {
+        event.preventDefault();
+
+        var location = $(this).val();
+        console.log("Cell: " + location + " was clicked.");
+
+
+    });
+});
+
+function doBoardUpdate(location)
+{
+    $.ajax(
+        {
+            datatype: 'json',
+            method: 'POST',
+            url: '/Minesweeper/OneCell',
+            data:
+            {
+                "location": location
+            },
+            success: function (data)
+            {
+                console.log(data)
+                $("#" + location).html(data);
+            }
+        });
+};
+
+function gameOver()
+{
+    $.ajax(
+        {
+            datatype: 'json',
+            method: 'GET',
+            url: 'Minesweeper/CheckGameOver',
+            data: {},
+            success: function (data)
+            {
+                console.log(data);
+                $('#gameOver').html(data);
+            }
+        });
+}
