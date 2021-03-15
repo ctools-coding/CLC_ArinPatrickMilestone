e8584bd9035a6e92b67a349e3f140e61de1b8f83
airisrare <pokerimproductions7@gmail.com>
2021-03-14 18:32:44 -0700

    game over in progress, running into an error

--- CLC_ArinPatrick/Services/Business/Game.cs
+++ CLC_ArinPatrick/Services/Business/Game.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 using Minesweeper_ArinPatrick.Models;
 
@@ -6,9 +7,30 @@ namespace Minesweeper_ArinPatrick.Services.Business
 {
     public class Game
     {
+        
+        int liveCount = 0;
+        int safeCount = 0;
+
         public bool gameOver(List<Cell> cellList)
         {
+            var liveCount = cellList.ElementAt(0).Live;
+            //bool gameOver = false;
+
+            for(int square = 1; square < cellList.Count; square++)
+            {
+                if(cellList.ElementAt(square) - safeCount == liveCount)
+                {
+                    return true;
+                }
+                else if(cellList.ElementAt(square).Visited == true && cellList.ElementAt(square).Live == true)
+                {
+                    
+                    return false;
+                }
+
+            }
             return false;
+            
         }
     }
 }
